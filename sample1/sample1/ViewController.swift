cbc73ce9255fb23254f2eeba4335e1b91833e438
Puru Pathak <puruppathak@gmail.com>
2017-04-03 12:02:34 -0400

    Creating a calculator

--- sample1/sample1/ViewController.swift
+++ sample1/sample1/ViewController.swift
@@ -10,13 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var label1: UILabel!
-    
-    @IBAction func button1pressed(_ sender: UIButton) {
-        
-        let title = sender.title(for: .Normal)!
-        label1.text="You clicked the \(title) button"
-    }
 
+    @IBAction func touchDigit(_ sender: UIButton) {
+        
+        print("Button was pressed")
+        
+    }
 }
 
