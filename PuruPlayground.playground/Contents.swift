742895c81510700b60453b86b8bc77fe6a7ee632
Puru Pathak <puruppathak@gmail.com>
2017-04-05 12:18:13 -0400

    Playing around with some code

---
+++ PuruPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var OptionalString : String? 
+var SayHi = "Hi there"
+
+if
+    let Isthere = OptionalString{
+    
+    SayHi="Hi there \(Isthere)"
+    
+}
+    
+    
+print(SayHi)
