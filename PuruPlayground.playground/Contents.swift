697c7f1be4969326b27bfcdbb4d164f8f874b605
Puru Pathak <puruppathak@gmail.com>
2017-04-05 14:36:07 -0400

    Playing around with some code

--- PuruPlayground.playground/Contents.swift
+++ PuruPlayground.playground/Contents.swift
@@ -14,8 +14,10 @@ if
     
 }
     
-    
-let vegetable = "red lettuce"
+
+let veg1="Green pepper"
+let vegetable = "red pepper"
+
 
 switch vegetable {
 case "celery":
@@ -26,7 +28,26 @@ case "cucumber", "watercress":
 
 case let x where x.hasSuffix("pepper"):
     let vegetableComment = "Is it a spicy \(x)"
-    
+    print(vegetableComment)
 default:
     let vegetableComment = "Everything tastes good in soup."
 }
+
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+var KindN="String"
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+            KindN=kind
+        }
+    }
+}
+print(largest)
+print(KindN)
