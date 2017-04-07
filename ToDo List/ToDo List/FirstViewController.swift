278cd30f6e73c906fdce0a22e931f840e6e993a7
Puru Pathak <puruppathak@gmail.com>
2017-04-07 14:43:46 -0400

    Started with the To do list app

---
+++ ToDo List/ToDo List/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ToDo List
+//
+//  Created by Puru Pathak on 4/7/17.
+//  Copyright © 2017 Puru Pathak. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
