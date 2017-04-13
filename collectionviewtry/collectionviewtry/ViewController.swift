a667ce48832b0af210f8b6bd4a577abaa70a4635
Puru Pathak <puruppathak@gmail.com>
2017-04-13 10:33:41 -0400

    general file deleted

---
+++ collectionviewtry/collectionviewtry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  collectionviewtry
+//
+//  Created by Puru Pathak on 4/13/17.
+//  Copyright © 2017 Puru Pathak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
